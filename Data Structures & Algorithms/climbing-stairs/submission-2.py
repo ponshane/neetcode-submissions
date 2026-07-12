e062271fb62b680aedf01b8983689f1f8e4d1601
Chia-Hsuan Chang <shane.chang.tw@gmail.com>
2026-07-12 10:10:37 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def dp_with_mem(self, n, mem):
+        # base case
+        if n <= 2:
+            return n
+        
+        if n in mem:
+            return mem[n]
+
+        res = self.dp_with_mem(n-1, mem) + self.dp_with_mem(n-2, mem)
+        mem[n] = res
+
+        return res
+
+    def climbStairs(self, n: int) -> int:
+        mem = {}
+        res = self.dp_with_mem(n, mem)
+        return res
+
+        
+    
