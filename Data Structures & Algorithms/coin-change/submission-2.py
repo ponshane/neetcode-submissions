e7a0c0c8b2144abac0fa291cef5afafa360a722d
Chia-Hsuan Chang <shane.chang.tw@gmail.com>
2026-07-12 11:29:34 -0400

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    # bottom-top approach
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        mem = {0: 0}
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                sub_amount = i - coin
+
+                if sub_amount < 0:
+                    continue # move to next coin
+                
+                if sub_amount not in mem:
+                    continue
+
+                candidate = mem[sub_amount] + 1 # use the current coin so + 1
+
+                if i not in mem:
+                    mem[i] = candidate
+                else:
+                    mem[i] = min(mem[i], candidate)
+        
+        return mem.get(amount, -1)
